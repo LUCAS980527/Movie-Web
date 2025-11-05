ed7e4aeb24b555c63f442c2f0bd09a73dd8dc86d
LUCAS980527 <amarbayn0527@gmail.com>
2025-11-05 11:51:53 +0800

    movie6

--- src/app/_features/Home/HeroSection.js
+++ src/app/_features/Home/HeroSection.js
@@ -1,5 +1,6 @@
-// import PlayIcon from "@/_Icons/Play Icon";
-// import StarIcon from "@/_Icons/StarIcon";
+"use clint";
+import PlayIcon from "@/_Icons/PlayIcon";
+import StarIcon from "@/_Icons/StarIcon";
 import {
   Carousel,
   CarouselContent,
@@ -16,9 +17,56 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const ACCESS_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjI5ZmNiMGRmZTNkMzc2MWFmOWM0YjFjYmEyZTg1NiIsIm5iZiI6MTc1OTcxMTIyNy43OTAwMDAyLCJzdWIiOiI2OGUzMGZmYjFlN2Y3MjAxYjI5Y2FiYmIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.M0DQ3rCdsWnMw8U-8g5yGXx-Ga00Jp3p11eRyiSxCuY";
 
 export default function HeroSection() {
   const images = [];
+  const [heroSectionData, setHeroSectionData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [selectedMovieId, setSelectedMovieId] = useState(null);
+  const [movieTrailer, setMovieTrailer] = useState({});
+  const [trailerLoading, setTrailerLoading] = useState(false);
+
+  const getHeroSectionData = async () => {
+    setLoading(true);
+    const heroSectionEndpoint = `${BASE_URL}/movie/now_playing?language=en-US&page=1`;
+    const response = await fetch(upcomingMovieEndpoint, {
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    setHeroSectionData(data.results);
+    setTimeout(() => {
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getHeroSectionData();
+  }, []);
+
+  const getMovieVideos = async () => {
+    setTrailerLoading(true);
+    const heroSectionEndpoint = `${BASE_URL}/movie/${selectedMovieId}/videos?language=en-US`;
+    const response = await fetch(upcomingMovieEndpoint, {
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
     <div className="bg-cover bg-center h-[600px] w-[1440px]">
       <Carousel className="w-full max-full">
@@ -27,8 +75,33 @@ export default function HeroSection() {
             <CarouselItem key={index}>
               <div className="p-1">
                 <Card>
-                  <CardContent className="flex aspect-square items-center justify-center h-[600px] w-[1440px]">
-                    <div className="bg-cover bg-center w-[1440px] h-[600px]"></div>
+                  <CardContent className="flex  items-center justify-center h-[600px] w-[1440px]">
+                    <div
+                      className="relative bg-cover bg-center bg-no-repeat h-[600px] w-full"
+                      style={
+                        {
+                          // backgroundImage: `url('https://image.tmdb.org/t/p/original${movie.backdrop_path}')`,
+                        }
+                      }
+                    >
+                      <div className=" h-[600px] w-[1440px] bg-cover bg-center bg-no-repeat align-center  mt-6">
+                        <div className=" flex w-[404px] h-[264px] ml-35 pt-45 gap-[16px] flex-col">
+                          <div className="w-[404px] h-[112px]">
+                            <div className="text-white font-inter font-normal text-[16px] leading-[24px] tracking-[0em]">
+                              Now Playing:
+                            </div>
+                            <StarIcon />
+                          </div>
+                          <div className="flex w-[145px] h-[40px] bg-white rounded-sm items-center justify-center py-2 px-4 gap-2">
+                            <PlayIcon />
+
+                            <button className="font-inter font-medium text-[14px] leading-[20px] tracking-[0]">
+                              Watch Trailer
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                     <span className="text-4xl font-semibold">{index + 1}</span>
                   </CardContent>
                 </Card>
@@ -42,32 +115,3 @@ export default function HeroSection() {
     </div>
   );
 }
-
-//     <div className=" bg-[url('/Feature.png')] h-[600px] w-[1440px] bg-cover bg-center bg-no-repeat align-center  mt-6">
-//       <div className=" flex w-[404px] h-[264px] ml-35 pt-45 gap-[16px] flex-col">
-//         <div className="w-[404px] h-[112px]">
-//           <div className="text-white font-inter font-normal text-[16px] leading-[24px] tracking-[0em]">
-//             Now Playing:
-//           </div>
-//           <h1 className="text-white font-inter font-extrabold text-[36px] leading-[40px] tracking-[-0.025em]">
-//             Wicked
-//           </h1>
-//           <StarIcon />
-//         </div>
-//         <div className="font-inter font-normal text-[12px] leading-[16px] tracking-[0em] text-white">
-//           Elphaba, a misunderstood young woman because of her green skin, and
-//           Glinda, a popular girl, become friends at Shiz University in the Land
-//           of Oz. After an encounter with the Wonderful Wizard of Oz, their
-//           friendship reaches a crossroads.{" "}
-//         </div>
-//         <div className="flex w-[145px] h-[40px] bg-white rounded-sm items-center justify-center py-2 px-4 gap-2">
-//           <PlayIcon />
-
-//           <button className="font-inter font-medium text-[14px] leading-[20px] tracking-[0]">
-//             Watch Trailer
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
